058201f8d2338e43383406ca15fef7656b4d080f
andres felipe <andresnarvaez304@gmail.com>
2025-08-26 10:43:04 -0500

    mi primer cambio

---
+++ app.ts
@@ -0,0 +1,21 @@
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+
+const user : User = {
+    id : 1,
+    name : "johh",
+    email : "johh@gmail.com"
+}
+
+console.log(user);
+
+
+
+
+
+
+
